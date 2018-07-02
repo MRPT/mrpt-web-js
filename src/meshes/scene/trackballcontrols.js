61d9387d368ff8b932548c042cd0c4c473ba64d3
rachit173 <rachittibrewal@outlook.com>
2018-07-02 18:33:37 +0530

    bug fixes in ws.js , trackballcontrol.js

--- src/meshes/scene/trackballcontrols.js
+++ src/meshes/scene/trackballcontrols.js
@@ -11,7 +11,7 @@ import window from 'window-or-global'
 var THREE = window.THREE || require('three');
 
 var TrackballControls;
-module.exports = TrackballControls = function ( object, domElement ) {
+export default TrackballControls = function ( object, domElement ) {
 
 	var _this = this;
 	var STATE = { NONE: - 1, ROTATE: 0, ZOOM: 1, PAN: 2, TOUCH_ROTATE: 3, TOUCH_ZOOM_PAN: 4 };
