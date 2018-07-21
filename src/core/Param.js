6eb8e3321a27897e8e0d3c56d551ed0c285ca69e
rachit173 <rachittibrewal@outlook.com>
2018-07-21 18:44:08 +0530

    increased doc coverage of core module

--- src/core/Param.js
+++ src/core/Param.js
@@ -1,3 +1,14 @@
+/**
+ * @class Param
+ * @todo: might be used in future for wrapping Param Object
+ *        for providing extended functionality, compression, defalte etc.
+ */
 export default class Param {
-
+  /**
+   * @constructor
+   * @param {object} params - contains the params to be sent in a message
+   */
+  constructor(params = {}) {
+    this.params = params;
+  }
 }
