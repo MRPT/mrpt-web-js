cf156af6d1658fe83f126990f01060396f97295b
rachit173 <rachittibrewal@outlook.com>
2018-07-22 21:37:32 +0530

    adding docs to plots module

--- src/plots/LinePlot.js
+++ src/plots/LinePlot.js
@@ -1,6 +1,27 @@
 import CartesianPlane from './CartesianPlane'
-
+/**
+ * create LinePlot or trace for LinePlot
+ * @example
+ * // adding standalone LinePlots to html containers
+ * let plot = new MRPTLIB.plots.LinePlot([1, 2, 3],[4, 5, 6],"some-html-container-id");
+ *
+ * // modifying plot data
+ * plot.processData([1, 3, 4, 5], [5, 6, 7, 9]);
+ *
+ * @example
+ * // trace for line plots
+ * let plot = new MRPTLIB.plots.CartesianPlane("some-html-container-id", trace0);
+ * let trace = new MRPTLIB.plots.LinePlot.processTrace([1, 3, 4],[2, 1, 4]);
+ * plot.addTrace(trace);
+ */
 export default class LinePlot extends CartesianPlane {
+  /**
+   * @constructor
+   * @param {Array<float>} [xs=[]] - x coords of points
+   * @param {Array<float>} [ys=[]] - y coords of points
+   * @param {string} divID - id of html container, to which lineplot is to be added
+   * @param {object} options - with the following keys
+   */
   constructor(xs = [], ys = [], divID, options = {})
   {
     let size = options.size || 3;
@@ -16,10 +37,10 @@ export default class LinePlot extends CartesianPlane {
   /**
    * processData : processes new points an replaces them
    * in the line plot, also alter the size and color of points
-   * @param {Array} xs
-   * @param {Array} ys
-   * @param {Number} size
-   * @param {*} color
+   * @param {Array<float>} xs
+   * @param {Array<float>} ys
+   * @param {?number} size - sets the marker size
+   * @param {?*} color
    */
   processData(xs = [], ys = [], size = null, color = null) {
     this.trace.x = xs;
--- src/plots/ScatterPlot.js
+++ src/plots/ScatterPlot.js
@@ -1,12 +1,26 @@
 import CartesianPlane from "./CartesianPlane";
-
+/**
+ * create ScatterPlot or trace for ScatterPlot
+ * @example
+ * // adding standalone ScatterPlots to html containers
+ * let plot = new MRPTLIB.plots.ScatterPlot([1, 2, 3],[4, 5, 6],"some-html-container-id");
+ *
+ * // modifying plot data
+ * plot.processData([1, 3, 4, 5], [5, 6, 7, 9], 2); // third parameter is the size of points
+ *
+ * @example
+ * // trace for line plots
+ * let plot = new MRPTLIB.plots.CartesianPlane("some-html-container-id", trace0);
+ * let trace = new MRPTLIB.plots.ScatterPlot.processTrace([1, 3, 4],[2, 1, 4]);
+ * plot.addTrace(trace); */
 export default class ScatterPlot extends CartesianPlane {
   /**
-   * @param {Array} xs - array containing x coords
-   * @param {Array} ys - array containing y coords
-   * @param {String} divID - the id of the container
-   * @param {Object} options - with the following keys
-   *    @param {Number} size - the size of the point, default is 3
+   * @constructor
+   * @param {Array<float>} [xs=[]] - array containing x coords
+   * @param {Array<float>} [ys=[]] - array containing y coords
+   * @param {string} divID - the id of the container
+   * @param {object} options - with the following keys
+   * @param {number} [options.size = 3] - the size of the point, default is 3
    */
   constructor(xs = [], ys = [], divID, options = {})
   {
@@ -23,10 +37,10 @@ export default class ScatterPlot extends CartesianPlane {
   /**
    * processData : processes new points an replaces them
    * in the scatter plot, also alter the size and color of points
-   * @param {Array} xs
-   * @param {Array} ys
-   * @param {Number} size
-   * @param {*} color
+   * @param {Array<float>} xs
+   * @param {Array<float>} ys
+   * @param {?number} size - sets the marker size
+   * @param {?*} color
    */
   processData(xs = [], ys = [], size = null, color = null) {
     this.trace.x = xs;
